11c9f473437270ead780bc35f11ed42d6804540e
Zeb <69901066+bfkjz@users.noreply.github.com>
2022-02-21 15:56:39 +0800

    once

---
+++ config.js
@@ -0,0 +1,26 @@
+//采集接口
+var vod_url_list={
+    "无尽资源":"https://api.wujinapi.com/api.php",
+    "快播云":"http://www.kuaibozy.com/api.php",
+    "天空云":"https://api.tiankongapi.com/api.php",
+    "百度资源":"https://api.apibdzy.com/api.php",
+    "爱播资源":"https://cj.apiabzy.com/api.php",
+    
+
+}
+
+//播放解析接口
+var play_url_list={
+    "百度资源":"https://jx.jxbdzyw.com/m3u8/?url=",
+    "解析2":"https://jx.7kjx.com/?url="
+}
+
+//代理请求接口
+var proxyurl = "http://apibf.herokuapp.com"; //http://127.0.0.1:1452/222/api.php
+
+//过滤排除加载类型
+var ex = ["资讯", "公告", "头条", "伦理片", "邵氏电影","三级片","体育赛事","伦理","论理","倫理","福利","激情","理论","写真","情色","美女","街拍","赤足","性感","里番","VIP"];
+
+//首页顶部导航条显示
+var ntn = ["电影", "连续剧", "综艺", "动漫"];
+
